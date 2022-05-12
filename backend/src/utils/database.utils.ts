3ec3a1571ec8c113e16734677e2e2192ef354e8e
Stephen Wade <swade6@cnm.edu>
2022-05-12 15:36:17 -0600

    Updated project.

---
+++ backend/src/utils/database.utils.ts
@@ -0,0 +1,17 @@
+import { Pool, createPool, PoolConnection } from 'mysql2/promise'
+let globalPool: Pool | undefined
+export async function connect (): Promise<PoolConnection> {
+  if (globalPool != null) {
+    return await globalPool.getConnection()
+  }
+  globalPool = await createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    connectionLimit: 10,
+    waitForConnections: true,
+    namedPlaceholders: true
+  })
+  return await globalPool.getConnection()
+}
