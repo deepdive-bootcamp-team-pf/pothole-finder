5f4e5b3f55596549b1d6c2af5898c96d7ce23a8f
Stephen Wade <swade6@cnm.edu>
2022-05-18 11:43:29 -0600

    added deletePhoto.ts

--- backend/src/utils/photos/deletePhoto.ts
+++ backend/src/utils/photos/deletePhoto.ts
@@ -4,7 +4,7 @@ import { RowDataPacket } from 'mysql2'
 
 export async function getAllPhoto() : Promise<Photo[]> {
     const mysqlConnection = await connect()
-    const mysqlQuery = 'select BIN_TO_UUID(photoID) as photoId, photoPotholeId, photoDate, photoDescription, photoName, photoURL from photo'
+    const mysqlQuery = 'delete from photo where photoId = UUID_TO_BIN(:photoId)'
     const result = await mysqlConnection.execute(mysqlQuery) as RowDataPacket[]
     await mysqlConnection.release()
     return result [0] as Photo[]
