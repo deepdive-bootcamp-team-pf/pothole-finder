8a865987253461b9d378bfbbd7f6e0e0003e7804
Deon Butler <deonbutler44@gmail.com>
2022-05-18 09:58:40 -1000

    profile.route.ts routes to various controllers.

--- backend/src/apis/profile/profile.route.ts
+++ backend/src/apis/profile/profile.route.ts
@@ -4,10 +4,11 @@ import {postProfileController,
         getAllProfileController,
         getProfileByProfileIdController,
         putProfileController} from './profile.controller'
+import { asyncValidatorController } from "../../utils/controllers/asyncValidator.controller";
 import { check } from 'express-validator'
 
 export const profileRoute = Router()
 
-profileRoute.route('/:profileId').get
+profileRoute.route('/:profileId').get(asyncValidatorController([check('profileId', 'Enter a valid profile ID').isUUID()]), getProfileByProfileIdController).put(asyncValidatorController([check('profileId', 'Enter a valid profile ID').isUUID()]), putProfileController).delete(asyncValidatorController([check('profileId', 'Enter a valid profile').isUUID()]), deleteProfileController)
 
-profileRoute.route('/')
+profileRoute.route('/').get(getAllProfileController).post(postProfileController)
