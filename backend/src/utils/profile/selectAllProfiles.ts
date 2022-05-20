32b9d88ca9f72c13a4818d096ab0484b9f0d0ace
Deon Butler <deonbutler44@gmail.com>
2022-05-20 06:43:18 -1000

    Sign up handler blessed off.

--- backend/src/utils/profile/selectAllProfiles.ts
+++ backend/src/utils/profile/selectAllProfiles.ts
@@ -4,7 +4,7 @@ import { RowDataPacket } from 'mysql2'
 
 export async function selectAllProfiles() : Promise<Profile[]> {
     const mysqlConnection = await connect()
-    const mysqlQuery = 'SELECT BIN_TO_UUID(profileId) AS profileId, profileAuthenticationToken, profileEmail, profileFirstName, profileHash, profileLastName, profileUsername FROM profile'
+    const mysqlQuery = 'SELECT BIN_TO_UUID(profileId) AS profileId, profileEmail, profileFirstName, profileLastName, profileUsername FROM profile'
     const result = await mysqlConnection.execute(mysqlQuery) as RowDataPacket[]
     await mysqlConnection.release()
     return result[0] as Profile[]
--- backend/src/utils/profile/selectProfileByProfileId.ts
+++ backend/src/utils/profile/selectProfileByProfileId.ts
@@ -4,7 +4,7 @@ import { RowDataPacket } from 'mysql2'
 
 export async function selectProfileByProfileId(profileId: string): Promise<Profile|null> {
     const mysqlConnection = await connect()
-    const mysqlQuery = 'SELECT BIN_TO_UUID(profileId) AS profileId, profileAuthenticationToken, profileEmail, profileFirstName, profileHash, profileLastName, profileUsername FROM profile WHERE profileId = UUID_TO_BIN(:profileId)'
+    const mysqlQuery = 'SELECT BIN_TO_UUID(profileId) AS profileId, profileEmail, profileFirstName, profileLastName, profileUsername FROM profile WHERE profileId = UUID_TO_BIN(:profileId)'
     const result = await mysqlConnection.execute(mysqlQuery, {profileId}) as RowDataPacket[]
     const profile: Profile[] = result[0] as Profile[]
     await mysqlConnection.release()
