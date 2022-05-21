9cc05cb69078ef02999f2d150006e78adef6ab21
Deon Butler <deonbutler44@gmail.com>
2022-05-20 22:15:59 -1000

    Sign up controller will notify user when their email account is already registered instead of defaulting to server error, functioning profile delete controller, remove profile function now takes Profile and PartialProfile as an argument.

--- backend/src/utils/profile/removeProfile.ts
+++ backend/src/utils/profile/removeProfile.ts
@@ -1,7 +1,7 @@
-import { Profile } from '../interfaces/Profile'
+import {PartialProfile, Profile} from '../interfaces/Profile'
 import { connect } from '../database.utils'
 
-export async function removeProfile(profile: Profile) : Promise<string> {
+export async function removeProfile(profile: Profile | PartialProfile) : Promise<string> {
     const mysqlConnection = await connect()
     const mysqlQuery = 'DELETE FROM profile WHERE profileId = UUID_TO_BIN(:profileId)'
     await mysqlConnection.execute(mysqlQuery, profile)
