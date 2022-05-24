264dcd660fbdbc0f4049536261cb8480d384be69
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-05-24 09:55:54 -0600

    added insert

--- backend/src/utils/pothole/insertPothole.ts
+++ backend/src/utils/pothole/insertPothole.ts
@@ -5,7 +5,7 @@ import { connect } from '../database.utils'
 
 export async function insertPothole(pothole: Pothole) : Promise<string> {
     const mysqlConnection = await connect()
-    const mysqlQuery = 'insert into pothole(potholeId, potholeProfileId,  potholeDate, potholeDescription, potholeLng, potholeLat, potholeSeverity) values (UUID_TO_BIN(UUID()))'
+    const mysqlQuery = 'insert into pothole(potholeId, potholeProfileId, potholeDate, potholeDescription, potholeLng, potholeLat, potholeSeverity) values (UUID_TO_BIN(UUID()))'
     await mysqlConnection.execute(mysqlQuery, pothole)
     await mysqlConnection.release()
     return 'Pothole uploaded successfully'
