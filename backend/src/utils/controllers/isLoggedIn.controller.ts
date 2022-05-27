30b16b10f43c40d412f755ae879d1992acf356f1
Deon Butler <deonbutler44@gmail.com>
2022-05-26 15:13:42 -1000

    isLoggedIn fix.

--- backend/src/utils/controllers/isLoggedIn.controller.ts
+++ backend/src/utils/controllers/isLoggedIn.controller.ts
@@ -5,17 +5,14 @@ import { Profile } from '../interfaces/Profile'
 import { IncomingHttpHeaders } from 'http'
 
 export function isLoggedIn (request: Request, response: Response, next: NextFunction): any {
-    const status: Status = {status: 400, message: 'Please login', data: null}
+    const status: Status = { status: 400, message: 'Please login', data: null }
 
-    // @ts-ignore
     const sessionProfile = (request: Request): Profile | undefined => request.session?.profile ?? undefined
-    // console.log(request.sessionID)
+    console.log(request.sessionID)
 
-    // @ts-ignore
     const signature = (request: Request): string => request.session?.signature ?? 'no signature'
 
     const isSessionActive = (isProfileActive: Profile | undefined): boolean => (isProfileActive !== undefined)
-
 
     const getJwtTokenFromHeader = (headers: IncomingHttpHeaders): string | undefined => {
         return headers.authorization
@@ -27,12 +24,18 @@ export function isLoggedIn (request: Request, response: Response, next: NextFunc
         if (unverifiedJwtToken === undefined) {
             return false
         }
+        const result: unknown = verify(
+            unverifiedJwtToken,
+            signature(request),
+            { maxAge: '3hr' },
+            (error: VerifyErrors | null): boolean => error == null
+        ) as unknown
 
-        const unverifiedJwtToken: string | undefined = getJwtTokenFromHeader(request.headers)
-
-        if (isJwtValid(unverifiedJwtToken) && isSessionActive(sessionProfile(request))) {
-            return next()
-        }
-        isJwtValid(unverifiedJwtToken) && isSessionActive(sessionProfile(request)) ? next() : response.json(status)
+        return result as boolean
     }
+
+    if (isJwtValid(unverifiedJwtToken) && isSessionActive(sessionProfile(request))) {
+        return next()
+    }
+    isJwtValid(unverifiedJwtToken) && isSessionActive(sessionProfile(request)) ? next() : response.json(status)
 }
