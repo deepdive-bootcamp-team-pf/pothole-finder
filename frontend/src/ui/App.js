12d413f62d960ffdb64e5365cf9d8da4a1de788e
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-06-01 16:04:19 -0600

    added navigation and severity page

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -2,16 +2,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
+import {SeverityPhotoPage} from "./SeverityPhotoPage"
+import { Navigation } from "./Navigation"
 import React from 'react'
+
+
 
 export const App = () => (
     <>
         <BrowserRouter>
+            <Navigation/>
             <Routes>
                 <Route  path='/' element={<Home />} />
                 <Route path='*' element={<FourOhFour />} />
-                <Route exact path='/SeverityPhotoPage' component={SeverityPhotoPage}/>
+                <Route exact path='/SeverityPhotoPage' component={<SeverityPhotoPage />} />
+
             </Routes>
+            <Form/>
         </BrowserRouter>
 
     </>
