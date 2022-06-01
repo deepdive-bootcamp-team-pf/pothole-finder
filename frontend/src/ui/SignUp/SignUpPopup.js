12268b7ba9250787c98e00f8cd5feeff3906df29
Stephen Wade <swade6@cnm.edu>
2022-06-01 16:12:05 -0600

    updated SignUpPopup.js

--- frontend/src/ui/SignUp/SignUpPopup.js
+++ frontend/src/ui/SignUp/SignUpPopup.js
@@ -11,7 +11,7 @@ export function SignUpPopup() {
             <button onClick={() => setButtonPopup(true)}>Sign Up</button>
             <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
                 <h2 className={'p-3 text-center'}>Sign Up</h2>
-                <p className={'m-0 p-0'}>Signing up is Free and easy.</p>
+                <p className={'m-0 p-0'}>Signing up is free and easy.</p>
                 <p className={'mt-0 mx-0 mb-5 p-0'}>Members can post potholes instantly!</p>
                 <SignUpForm/>
             </Popup>
