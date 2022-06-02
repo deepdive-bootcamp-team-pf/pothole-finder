b954caf800c1673529f8f224283c1046de5e476f
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-06-02 14:58:00 -0600

    merge

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -1,38 +1,23 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
-<<<<<<< HEAD
 import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
 import {SeverityPhotoPage} from "./SeverityPhotoPage"
 import { Navigation } from "./Navigation"
 import React from 'react'
-
-
-=======
-import {Home} from './Home'
-import {FourOhFour} from './FourOhFour'
-import React from 'react'
-
 import {SignUpPopup} from "./SignUp/SignUpPopup";
->>>>>>> 9ef4d8caad31657079d052a5916640b3dc4ef3fb
 
 export const App = () => (
     <>
         <BrowserRouter>
             <Navigation/>
             <Routes>
-<<<<<<< HEAD
-                <Route  path='/' element={<Home />} />
-                <Route path='*' element={<FourOhFour />} />
-                <Route exact path='/SeverityPhotoPage' component={<SeverityPhotoPage />} />
 
-=======
+                <Route exact path='/SeverityPhotoPage' component={<SeverityPhotoPage />} />
                 <Route path='/' element={<Home/>}/>
                 <Route path='/404' element={<FourOhFour/>}/>
                 <Route path='/signup' element={<SignUpPopup/>}/>
->>>>>>> 9ef4d8caad31657079d052a5916640b3dc4ef3fb
             </Routes>
-            <Form/>
         </BrowserRouter>
     </>
 )
