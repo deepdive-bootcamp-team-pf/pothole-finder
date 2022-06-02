82c49660e4737b4b9d91b9665d728367ff6b641a
Stephen Wade <swade6@cnm.edu>
2022-06-02 15:29:20 -0600

    updated Home.js and Popup.css; added HomeNav.css

--- frontend/src/ui/home/Home.js
+++ frontend/src/ui/home/Home.js
@@ -2,13 +2,17 @@ import React from 'react'
 import Map from './Map'
 import { SignUpPopup } from '../SignUp/SignUpPopup'
 import {Button, Col, Container, Row} from 'react-bootstrap'
+import './HomeNav.css'
 
 export function Home() {
     return (
         <>
-            <Container fluid className={'d-flex'}>
-                <Row>
-                    <Col className={'d-inline-flex justify-content-end'}>
+            <Container fluid className={'home-nav position-absolute bg-transparent'}>
+                <Row className={'d-flex pt-3'}>
+                    <Col className={'mr-auto'}>
+                        <Button className={'m-3'} size={'lg'}>Add Pothole</Button>
+                    </Col>
+                    <Col className={'d-flex justify-content-end'}>
                         <Button className={'m-3'} size={'lg'}>Log In</Button>
                         <SignUpPopup/>
                     </Col>
