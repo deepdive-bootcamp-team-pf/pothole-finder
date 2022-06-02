fa7a08d75d0f402e3fd0fd9b408a278a3d1d8fa3
Stephen Wade <swade6@cnm.edu>
2022-06-02 13:15:54 -0600

    updated Popup.css

--- frontend/src/ui/SignUp/Popup.js
+++ frontend/src/ui/SignUp/Popup.js
@@ -5,7 +5,7 @@ import {Container} from "react-bootstrap";
 export function Popup(props) {
     return (props.trigger) ? (
             <Container fluid className={'popup d-flex justify-content-center align-items-center'}>
-                <Container fluid className={'popup-inner'}>
+                <Container fluid className={'popup-inner rounded border'}>
                     <button className={'close-btn'} onClick={() => props.setTrigger(false)}>close</button>
                     {props.children}
                 </Container>
