2d2cc6f50b4b6926225f174494cc9aa8fbf01912
Deon Butler <deonbutler44@gmail.com>
2022-06-02 10:36:13 -1000

    Login and Signup button merged into Home page.

--- frontend/src/ui/home/Home.js
+++ frontend/src/ui/home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Map from './Map'
 import { SignUpPopup } from '../SignUp/SignUpPopup'
+import {Button, Container} from 'react-bootstrap'
 
 export function Home() {
     return (
         <>
-            <SignUpPopup/>
+            <Container fluid>
+                <Button className={'m-3'} size={'lg'}>Log In</Button>
+                <SignUpPopup/>
+            </Container>
             <Map/>
         </>
     )
