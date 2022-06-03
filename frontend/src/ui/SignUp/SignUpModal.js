004327bc232ac9d326ee483512d4649367291cca
Stephen Wade <swade6@cnm.edu>
2022-06-03 12:31:08 -0600

    formated Modal

--- frontend/src/ui/SignUp/SignUpModal.js
+++ frontend/src/ui/SignUp/SignUpModal.js
@@ -12,13 +12,13 @@ export function SignUpModal(props) {
             centered
         >
             <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter"  className={'text-center'}>
+                <Modal.Title id="contained-modal-title-vcenter"  className={'text-center ps-3'}>
                     <h2> Sign Up</h2>
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <p className={'m-0 p-0'}>Signing up is free and easy.</p>
-                <p className={'mt-0 mx-0 mb-5 p-0'}>Members can post potholes instantly!</p>
+                <p className={'m-0 ps-3'}>Signing up is free and easy.</p>
+                <p className={'mt-0 mx-0 mb-5 ps-3'}>Members can post potholes instantly!</p>
                 <SignUpForm/>
             </Modal.Body>
         </Modal>
