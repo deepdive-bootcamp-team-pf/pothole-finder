154ec97ad9c04a4ddf8a441dbc652e69b74c232f
Deon Butler <deonbutler44@gmail.com>
2022-06-06 11:30:25 -1000

    Housekeeping/ file uniformity.

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -1,20 +1,18 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
-import { Home } from './home/Home'
+import { Home } from './Home/Home'
 import { FourOhFour } from './FourOhFour'
-import {SeverityPhotoPage} from "./Severity/SeverityPhotoPage"
+import {SeverityPhotoPage} from './Severity/SeverityPhotoPage'
 import React from 'react'
-import {SignUpModal} from "./SignUp/SignUpModal"
-
-import {Provider} from "react-redux"
-
+import {SignUpModal} from './SignUp/SignUpModal'
+import {Provider} from 'react-redux'
 
 export const App = (store) => (
     <Provider store={store}>
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home/>}/>
-                <Route path='/404' element={<FourOhFour/>}/>
+                <Route path='*' element={<FourOhFour/>}/>
                 <Route path='/signup' element={<SignUpModal/>}/>
                 <Route path='/severity-photo-page' element={<SeverityPhotoPage/>}/>
             </Routes>
--- frontend/src/ui/SignUp/SignUpModal.js
+++ frontend/src/ui/SignUp/SignUpModal.js
@@ -1,6 +1,6 @@
-import React from "react"
-import {SignUpForm} from "./SignUpForm";
-import {Container, Modal} from "react-bootstrap";
+import React from 'react'
+import {SignUpForm} from './SignUpForm'
+import {Container, Modal} from 'react-bootstrap'
 import './SignUp.css'
 
 
@@ -19,8 +19,8 @@ export function SignUpModal(props) {
             </Modal.Header>
             <Modal.Body>
                 <Container fluid className={"pt-3 background-image"}>
-                    <p className={'m-0 ps-3 text-white'}>Signing up is free and easy.</p>
-                    <p className={'mt-0 mx-0 mb-5 ps-3 text-white'}>Members can post potholes instantly!</p>
+                    <p className={'m-0 ps-3 text-white text-on-photo'}>Signing up is free and easy.</p>
+                    <p className={'mt-0 mx-0 mb-5 ps-3 text-white text-on-photo'}>Members can post potholes instantly!</p>
                 </Container>
                 <SignUpForm/>
             </Modal.Body>
