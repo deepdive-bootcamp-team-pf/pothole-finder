6e83d6d3a9883901344872ac0d1580dc8ae39d76
Stephen Wade <swade6@cnm.edu>
2022-06-08 13:05:34 -0600

    changed pin placement for home screen

--- frontend/src/ui/Home/Map.js
+++ frontend/src/ui/Home/Map.js
@@ -4,34 +4,30 @@ import './Map.css'
 import Map from 'react-map-gl'
 import mapLibre from 'maplibre-gl'
 import { Pin } from './Pin'
-// import mapLib from "mapbox-gl";
 
 export default function MapFunction() {
 
     const [points] = React.useState([
-        { lat: 35.332, lng: -106.652 },
-        { lat: 35.339, lng: -106.656 },
-        { lat: 35.40, lng: -106.666 },
-        { lat: 35.23, lng: -106.4444 }
+        { lat: 35.132088, lng: -106.657851},
+        { lat: 35.152941, lng: -106.643294 },
+        { lat: 35.138914, lng: -106.694326},
+        { lat: 35.23, lng: -106.4444}
     ])
 
     return (
         <>
                 <Map
-                    // className="map"
-                    // ref={mapContainer}
+
                     mapLib={mapLibre}
                     initialViewState={{
-                        latitude: 35.33,
-                        longitude: -106.65,
-                        zoom: 9
+                        latitude: 35.0844,
+                        longitude: -106.6504,
+                        zoom: 12
                     }}
                     style={{height: '100vw'}}
                     mapStyle="https://api.maptiler.com/maps/streets/style.json?key=D4b2ldjY7geFrPnuBPU8"
                 >
                     {points.map((point, index) => <Pin lat={point.lat} lng={point.lng} index={index} key={index}/>)}
-
-
                 </Map>
         </>
     )
