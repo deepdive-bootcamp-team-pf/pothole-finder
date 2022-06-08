20d515c2604f93c9f905d9833ac06de84606dfdf
Stephen Wade <swade6@cnm.edu>
2022-06-08 10:21:20 -0600

    added emojis to Pin.js

--- frontend/src/ui/Home/Map.js
+++ frontend/src/ui/Home/Map.js
@@ -1,37 +1,24 @@
-import React, {useRef, useEffect, useState} from 'react'
-import 'maplibre-gl/dist/maplibre-gl.css'
+import React from 'react'
+// import 'maplibre-gl/dist/maplibre-gl.css'
 import './Map.css'
-import {fetchAllPotholes} from "../../store/potholes";
 import Map from 'react-map-gl'
 import mapLibre from 'maplibre-gl'
-import { Container, Row, Col } from 'react-bootstrap'
 import { Pin } from './Pin'
 
-export default function Map(){
+export default function Map() {
 
-    // // Tell this component that it needs to watch for items that live outside this component (misquotes in the redux store)
-    // // This is how we make sure the component looks for our data from redux and the fetch response from the backend
-    // const dispatch = useDispatch()
-    // const initialEffects = () => {
-    //     dispatch(fetchAllPotholes())
-    // }
-    // React.useEffect(initialEffects, [dispatch])
-    //
-    // // use the misquotes data from the store
-    // const potholes = useSelector((state) => state.potholes ? state.potholes : [])
-    //
-    // console.log(potholes)
     const [points] = React.useState([
         { lat: 35.332, lng: -106.652 },
         { lat: 35.339, lng: -106.656 },
         { lat: 35.40, lng: -106.666 },
         { lat: 35.23, lng: -106.4444 }
+    ])
 
 
     return (
         <div className="map-wrap">
             <Map
-                ref={mapContainer}
+                // ref={mapContainer}
                 className="map"
                 mapLib={mapLibre}
                 initialViewState={{
--- frontend/src/ui/Home/Pin.js
+++ frontend/src/ui/Home/Pin.js
@@ -1,5 +1,7 @@
 import {Marker} from "react-map-gl";
-import bomb from './bomb.png'
+import bomb from './icons/bomb.png'
+import crying from './icons/crying.png'
+import facePalm from './icons/face-palm.png'
 export function Pin(props) {
     const {lat, lng, index} = props
 
@@ -13,5 +15,7 @@ export function Pin(props) {
     return (
         <Marker key={`marker-${index}`} longitude={lng} latitude={lat}>
             <img src={bomb} alt="" style={{width: '25px', height: '25px'}}/>
+            <img src={crying} alt="" style={{width: '25px', height: '25px'}}/>
+            <img src={facePalm} alt="" style={{width: '25px', height: '25px'}}/>
         </Marker>)
 }
