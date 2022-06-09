41f23100ee5cc41d4bc6a3c779195b82e15b7d76
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-06-09 15:31:02 -0600

    changed proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
     app.use(createProxyMiddleware('/apis', {
         logLevel: 'debug',
         //ip address or domain goes here (you will put your ip address in line 7)
-        target: "http://138.68.252.84:8080",
+        target: "http://165.227.0.138:8080",
         changeOrigin: true,
         secure: true,
-    }))
-}
+    }));
+};
